bb93838ed8033d779a76580a4df4496466716c80
Chris McAloney <chris.mcaloney@nokia.com>
2023-05-26 09:22:45 -0300

    move volume up to left side, disable left-side enter key

--- keyboards/redox/keymaps/mcaloney/keymap.c
+++ keyboards/redox/keymaps/mcaloney/keymap.c
@@ -26,6 +26,7 @@ enum custom_keycodes {
 #define KC_NAGR LT(_NAV, KC_GRV)
 #define KC_NAMI LT(_NAV, KC_MINS)
 
+#define KC_SYVU LT(_SYMB, KC_VOLU)
 #define KC_ADVD LT(_ADJUST, KC_VOLD)
 #define KC_ADVU LT(_ADJUST, KC_VOLU)
 
@@ -39,9 +40,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_CTES ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_LBRC ,                          KC_RBRC ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADVD ,SYM_L   ,        SYM_L   ,KC_ADVU ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
+     KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_ADVD ,KC_SYVU ,        SYM_L   ,KC_ADVU ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LCTL ,KC_PPLS ,KC_PMNS ,KC_ALBS ,     KC_LGUI ,    KC_BSPC ,KC_ENT  ,        KC_ENT  ,KC_SPC  ,    KC_RALT ,     KC_HOME ,KC_END  ,KC_PGUP ,KC_PGDN 
+     KC_LCTL ,KC_PPLS ,KC_PMNS ,KC_ALBS ,     KC_LGUI ,    KC_BSPC ,XXXXXXX ,        KC_ENT  ,KC_SPC  ,    KC_RALT ,     KC_HOME ,KC_END  ,KC_PGUP ,KC_PGDN 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
